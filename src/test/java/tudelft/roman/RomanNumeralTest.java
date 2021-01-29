a6af547aaf40c08d5d8e8c08ba4d20d68e22ba8b
Cor van der Velde <Cor.vanderVelde@epay.ingenico.com>
2021-01-29 09:36:11 +0100

    Added tests till chocolate test

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,35 +1,45 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void inialize() {
+        this.roman = new RomanNumeral();
+    }
+
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
+    public void numberWithLargeDigits() {
+        int result = roman.convert("MD");
+        Assertions.assertEquals(1500, result);
+    }
+
+    @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
